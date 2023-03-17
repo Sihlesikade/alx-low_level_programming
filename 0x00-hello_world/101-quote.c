25715fbf6fe788fa125834a1a3210566e142b068
siphosihle <sikadesihle@gmail.com>
2023-03-17 06:01:22 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c  program that print a line to the standard error
@@ -8,4 +8,4 @@ int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-ii}
+}
