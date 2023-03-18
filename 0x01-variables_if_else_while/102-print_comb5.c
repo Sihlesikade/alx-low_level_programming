35b422b087a7ff883397e559230c6ab5dadbd692
siphosihle <sikadesihle@gmail.com>
2023-03-18 06:36:59 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
@@ -15,13 +16,13 @@ int main(void)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
-				putchar('');
+				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
-					putchar('');
+					putchar(' ');
 				}
 			}
 		}
