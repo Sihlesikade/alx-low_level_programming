b99a6fadf487e8b6bcc1f1c5cbc4bf08c6d03c02
siphosihle <sikadesihle@gmail.com>
2023-03-21 10:55:07 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 {
 
 	int x, y, z, u, d;
-	
+
 	for (x = 0; x <= 0; x++)
 	{
 	for (y = 0; y <= 9; y++)
@@ -25,7 +25,7 @@ void times_table(void)
 	_putchar(d + '0');
 	_putchar(u + '0');
 	}
-	else 
+	else
 	{
 	if (y != 0)
 	{
