4b241c0dbf9d332de013deb04992a44d5d9401c6
siphosihle <sikadesihle@gmail.com>
2023-03-21 23:38:20 -0700

    task14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	print("%ld", j);
+	printf("%ld", j);
 	else if (i == 1)
 	printf(", %ld", k);
 	else
 	{
 	k += j;
-	j = k - j);
+	j = k - j;
 	printf(", %ld", k);
 	}
 	++i;
