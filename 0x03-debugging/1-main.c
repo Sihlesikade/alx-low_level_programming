eafe6308d8fc222032b099655f25f04a73507fbc
siphosihle <sikadesihle@gmail.com>
2023-03-23 16:14:46 -0700

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 	i = 0;
 
 	/**
-	 * while (1 < 10)
-	 * {
-	 * putchar(i)
-	 * }
+	 * while (i < 10)
+	 *}
+	 * putchar(i);
+	 *}
 	 */
 	printf("Infinite loop avoid! \\o/\n");
 
