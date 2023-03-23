2d2d2abf3e7162e59e91b05f9e5e879c626ea874
siphosihle <sikadesihle@gmail.com>
2023-03-23 04:37:02 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,17 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	i = 0;
 	/*
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
-	printf("infinite loop avoided! \\o/\n");
+	 *
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	*/
+	printf("Infinite loop avoid! \\o/\n");
 
 	return (0);
 }
