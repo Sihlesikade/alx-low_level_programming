e44871d50f3b5f9e7f4cd091008b8bcd3e8f8887
siphosihle <sikadesihle@gmail.com>
2023-03-23 17:36:51 -0700

    task8

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,17 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @c: number of times the character _ should be printed
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-	if (n <= 0);
+	if
+
+		(n <= 0);
 	{
 		_putchar('\n');
-	} else
+	}
+	else
 	{
 		int i;
 
