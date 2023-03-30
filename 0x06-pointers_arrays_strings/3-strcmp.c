aacfc93e7bf30fe021a0077a7fd1287c5583ac35
siphosihle <sikadesihle@gmail.com>
2023-03-29 21:19:51 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i:
+	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[1] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
